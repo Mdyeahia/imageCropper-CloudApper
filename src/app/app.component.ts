4a80350607f05692bc9204a5d8de17824c0361b5
Yeahia.khan <Yeahia.khan@kdsgroup.net>
2024-05-13 17:38:38 +0600

    project publish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { WebcamImage } from 'ngx-webcam';
+import { Observable, Subject } from 'rxjs';
+import { ImageCroppedEvent } from 'ngx-image-cropper';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'imageCropper';
+  @ViewChild('webcamElement') webcamElement?: ElementRef<HTMLVideoElement>;
+  trigger: Subject<any> = new Subject();
+  isWebcamOn: boolean = false;
+  webcamImage: any = '';
+  sysImage = '';
+  imageChangedEvent: any;
+  croppedImage: any = '';
+
+
+  toggleWebcam(): void {
+    this.isWebcamOn = !this.isWebcamOn;
+    if (this.isWebcamOn) {
+      this.startWebcam();
+    } else {
+      this.stopWebcam();
+      this.webcamImage = '';
+    }
+  }
+
+  startWebcam(): void {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        if (this.webcamElement) {
+          this.webcamElement.nativeElement.srcObject = stream;
+        }
+      })
+      .catch((err) => console.error('Error starting webcam:', err));
+  }
+
+  stopWebcam(): void {
+    if (this.webcamElement && this.webcamElement.nativeElement.srcObject) {
+      const stream = this.webcamElement.nativeElement.srcObject as MediaStream;
+      const tracks = stream.getTracks();
+      tracks.forEach((track) => track.stop());
+      this.webcamElement.nativeElement.srcObject = null;
+    }
+  }
+
+  public captureImg(webcamImage: WebcamImage): void {
+
+    this.webcamImage = webcamImage;
+  }
+
+  public getSnapshot(): void {
+    this.trigger.next(void 0);
+  }
+
+
+  public get invokeObservable(): Observable<any> {
+    return this.trigger.asObservable();
+  }
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.objectUrl;
+  }
+  saveImageToDesktop() {
+    const link = document.createElement('a');
+    link.href = this.croppedImage;
+    link.download = 'cropped_image.jpeg';
+    link.click();
+  }
 }
