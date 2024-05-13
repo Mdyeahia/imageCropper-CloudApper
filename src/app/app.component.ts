6878a9c4ac52475d0cf3f45b077be339fb50b436
Yeahia.khan <Yeahia.khan@kdsgroup.net>
2024-05-13 17:56:55 +0600

    pdf or image option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { WebcamImage } from 'ngx-webcam';
 import { Observable, Subject } from 'rxjs';
 import { ImageCroppedEvent } from 'ngx-image-cropper';
+import jsPDF from 'jspdf';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -64,9 +66,37 @@ export class AppComponent {
     this.croppedImage = event.objectUrl;
   }
   saveImageToDesktop() {
+
+    Swal.fire({
+      title: "Save Image",
+      text: "Would you like to save the image as a PDF or Image?",
+      showCancelButton: true,
+      confirmButtonText: "PDF",
+      cancelButtonText: "Image"
+    }).then((result) => {
+
+      if (result.isConfirmed) {
+
+        this.saveAsPdf();
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+
+        this.saveAsJpeg();
+      }
+    });
+  }
+
+
+  private saveAsJpeg() {
+
     const link = document.createElement('a');
     link.href = this.croppedImage;
     link.download = 'cropped_image.jpeg';
     link.click();
+  }
+
+  private saveAsPdf() {
+    const doc = new jsPDF();
+    doc.addImage(this.croppedImage, 'JPEG', 10, 10, 100, 100);
+    doc.save('cropped_image.pdf');
   }
 }
